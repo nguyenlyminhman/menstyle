a9f9ca6d3f3d523e94542ab386c9ed628cfbe9fb
nguyenlyminhman <nguyenlyminhman@gmail.com>
2017-08-05 13:08:13 +0700

    create customer router

---
+++ controller/customerRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+//define the home page router
+router.get('/', require('./showHomePage'));
+router.get('/contact', require('./showContactPage'));
+router.get('/checkout', require('./showCheckOutPage'));
+router.get('/single', require('./showSinglePage'));
+router.get('/account', require('./showAccountPage'));
+router.get('/register', require('./showRegisterPage'));
+router.get('/products', require('./showProductsPage'));
+
+router.get('/category/:cate_seolink/:idcategory', require('./showProductsPage'));
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const app = express();
-const Product = require('./model/Product')
+const customer = require('./controller/customerRouter')
 
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 app.use('admin', express.static('public/admin'));
+app.use('/', customer);
 
-app.get('/', require('./controller/showHomePage'));
-app.get('/contact', require('./controller/showContactPage'));
-app.get('/checkout', require('./controller/showCheckOutPage'));
-app.get('/single', require('./controller/showSinglePage'));
-app.get('/account', require('./controller/showAccountPage'));
-app.get('/register', require('./controller/showRegisterPage'));
-app.get('/products', require('./controller/showProductsPage'));
+// app.get('/', require('./controller/showHomePage'));
+// app.get('/contact', require('./controller/showContactPage'));
+// app.get('/checkout', require('./controller/showCheckOutPage'));
+// app.get('/single', require('./controller/showSinglePage'));
+// app.get('/account', require('./controller/showAccountPage'));
+// app.get('/register', require('./controller/showRegisterPage'));
+// app.get('/products', require('./controller/showProductsPage'));
 
-app.get('/products/category_type/:idcategory', require('./controller/showProductsPage'));
+// app.get('/products/:cate_seolink/:idcategory', require('./controller/showProductsPage'));
 
 
 // app.get('/products/category_type/:idcategory', (req, res) => {
